d6703be8a3a176c8c140e27f73d046eb149b73fe
Domaslal <151125549+Domaslal@users.noreply.github.com>
2026-02-20 09:46:16 +0200

    Hero title changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Dokumentų valdymo sistema​ | DVS įrankis | Vecticum";
-export const SITE_DESCRIPTION = "Dokumentų valdymo sistema​, automatizuoti verslo procesų sprendimai: personalo, dokumentų, sąskaitų, verslo procesų valdymas ir daugiau. Išbandykite dabar!";
+export const SITE_TITLE = "Dokumentų, sąskaitų ir personalo valdymo sistema​ | DVS įrankis | Vecticum";
+export const SITE_DESCRIPTION = "Dokumentų, sąskaitų ir personalo valdymo sistema​, automatizuoti verslo procesų sprendimai: personalo, dokumentų, sąskaitų, verslo procesų valdymas ir daugiau. Išbandykite dabar!";
 export const OMNISEND_BRAND_ID = "654b561447acf802c1017b43";// Replace with your actual Omnisend Brand ID
 
 export const CLIENTS = [
